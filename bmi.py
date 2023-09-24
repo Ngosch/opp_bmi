9079f43546dddfffa757f52b821928c377bad0fe
Ngosch <carl1312@gmail.com>
2023-09-24 18:13:41 +0900

    とりあえずBMI

---
+++ bmi.py
@@ -0,0 +1,21 @@
+
+class BMI:
+    def __init__(self, height, weight):
+        self.height = height  # インスタンス変数
+        self.weight = weight  # インスタンス変数
+
+    # インスタンスメソッドの作成
+    def calculate_bmi(self):
+        # BMIの計算式を返す 体重÷身長2乗
+        return self.weight / (self.height ** 2)
+
+
+# BMIクラスのインスタンス化
+tanaka_bmi = BMI(height=1.80, weight=67.0)
+sasami_bmi = BMI(height=1.58, weight=80.0)
+
+print(tanaka_bmi.height, sasami_bmi.height)
+
+# calculate_bmiメソッドの出力
+print(tanaka_bmi.calculate_bmi())
+print(sasami_bmi.calculate_bmi())
